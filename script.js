160688574330d5f8ac03443d9b9456071452d150
Brendo <brendowashingtone@gmail.com>
2024-08-30 09:16:16 -0300

    j

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
 const firstName = document.getElementById("firstName");
 const lastName = document.getElementById("lastName");
 
+
 const submit = document.getElementById("submit");
 
-submit.addEventListener("click", verificarNomes);
+submit.addEventListener("click", checkLastName);
 
-function verificarNomes() {
-  if (firstName.value == "" || lastName.value == "") {
-    alert("Digite um nome");
+function checkFirstName() {
+  if (firstName.value == "") {
+    let erro = document.getElementById('erroFName');
+    erro.innerHTML = 'oi';
   } else {
-    alert("tudo ik");
+    erro.innerHTML = 'dfdfd';
+  }
+}
+
+function checkLastName(event) {
+  event.preventDefault()
+  if (lastName.value == "") {
+    let erro = document.getElementById('erroLName');
+    erro.innerHTML = 'oi';
+  } else {
+    erro.innerHTML = 'dfdfd';
+  }
+  
+}
+
+function checkEmail() {
+  const email = document.getElementById('email');
+  let mail = email.value;
+  if(mail.includes('@gmail.com') && mail.indexOf('@gmail.com') == mail.length - 10) {
+    alert('foi')
+    let 
+  } else {
+    alert('sddsd')
   }
 }
